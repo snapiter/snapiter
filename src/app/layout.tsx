794ada6ed4c9563db13cc7ccd5ce8ff48fe52ea2
arnovr <arnovanrossum1@gmail.com>
2025-08-27 18:51:07 +0200

    text

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "SnapIter",
-  description: "Snap your journey",
+  description: "Snap your Iter",
 };
 
 export default function RootLayout({
