26cc4ead7cecabe6c58998ff3baa410ff801e286
arnovr <arnovanrossum1@gmail.com>
2025-08-28 11:41:47 +0200

    Env

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
+  env: {
+    NEXT_PUBLIC_MAPTILER_KEY: process.env.NEXT_PUBLIC_MAPTILER_KEY,
+  },
   images: {
     remotePatterns: [
       {
