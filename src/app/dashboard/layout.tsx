d1aafbcbc8dd25782b92e0f23c9324a1911173d9
arnovr <arnovanrossum1@gmail.com>
2025-09-17 13:59:44 +0200

    Split up the dashboard routes, either for authentication or protected ( logged in routes )

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,41 +1,7 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "../globals.css";
-import Header from "@/components/dashboard/Header";
-import Footer from "@/components/dashboard/Footer";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
-  title: "SnapIter",
-  description: "Snap your Iter",
-};
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-
-<Header />
-        <main>
-        {children}
-        </main>
-        <Footer />
-      </body>
-    </html>
-  );
+export default function Layout({ children }: { children: React.ReactNode }) {
+    return (
+        <>
+            {children}
+        </>
+    );
 }
