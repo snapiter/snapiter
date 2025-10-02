9154721efda339c44d0875e78a6d86f45e945f5a
arnovr <arnovanrossum1@gmail.com>
2025-10-02 13:21:09 +0200

    Remove cache from snapiter. Its not a proper way of doing things

--- next.config.ts
+++ next.config.ts
@@ -13,12 +13,6 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
-      {
-        protocol: 'https',
-        hostname: 'cache.snapiter.com',
-        port: '',
-        pathname: '/**',
-      },
     ],
   },
 };
