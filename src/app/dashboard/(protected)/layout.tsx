9073a76bdbfa29d81599b4f657165764ca57b34b
arnovr <arnovanrossum1@gmail.com>
2025-10-07 13:31:55 +0200

    Remove duplicate layout and fix env for dashboard

--- src/app/dashboard/(protected)/layout.tsx
+++ src/app/dashboard/(protected)/layout.tsx
@@ -6,6 +6,12 @@ import LoadingBar from "@/components/dashboard/layout/LoadingBar";
 import ErrorBox from "@/components/dashboard/layout/ErrorBox";
 import { Provider } from "jotai";
 import { Footer } from "@snapiter/designsystem";
+import EnvProvider from "@/utils/env/EnvProvider";
+import QueryProvider from "@/components/QueryProvider";
+import getEnv from "@/utils/env/getEnv";
+import "../dashboard.css";
+import { ENV_PREFIX } from "@/utils/env/envprefix";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,20 +33,35 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const env = getEnv();
+
   return (
     <html lang="en">
+      <head>
+      <Script
+          id="runtimeEnv"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `window.${ENV_PREFIX}=${JSON.stringify(env)};`
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
       >
-        <Provider>
-          <Header />
-          <LoadingBar />
-          <ErrorBox />
-          <div className="flex flex-1 relative">
-            {children}
-          </div>
-          <Footer withMenu={false} />
-        </Provider>
+        <EnvProvider value={env}>
+          <QueryProvider>
+            <Provider>
+              <Header />
+              <LoadingBar />
+              <ErrorBox />
+              <div className="flex flex-1 relative">
+                {children}
+              </div>
+              <Footer withMenu={false} />
+            </Provider>
+          </QueryProvider>
+        </EnvProvider>
       </body>
     </html>
   );
