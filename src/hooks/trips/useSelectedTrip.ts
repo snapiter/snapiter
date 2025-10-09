dd2d90549dc97477781a79d25107f65ea573a68f
arnovr <arnovanrossum1@gmail.com>
2025-10-09 11:01:47 +0200

    simplified the hooks a bit

--- src/hooks/trips/useSelectedTrip.ts
+++ src/hooks/trips/useSelectedTrip.ts
@@ -1,14 +1,14 @@
 import { selectedTripAtom } from "@/store/atoms";
 import { useAtomValue } from "jotai";
 import { useTripsByHostname } from "./useTripsByHostname";
-import { useDetailedTrip } from "./useTrip";
+import { useTrip } from "./useTrip";
 
 export function useSelectedTrip() {
     const selectedTripSlug = useAtomValue(selectedTripAtom);
     const { data: trips, isSuccess: tripsSuccess } = useTripsByHostname();
     const selectedTrip = trips?.find(t => t.slug === selectedTripSlug);
 
-    const { data: trip, isSuccess: tripSuccess } = useDetailedTrip(selectedTrip ?? null);
-    
+    const { data: trip, isSuccess: tripSuccess } = useTrip(selectedTrip ?? null);
+
     return { trip: tripSuccess && tripsSuccess ? trip : null }
  }
--- src/hooks/trips/useTrip.ts
+++ src/hooks/trips/useTrip.ts
@@ -4,62 +4,35 @@ import { useApiClient } from '../useApiClient';
 
 export function useTripWithPosition(trackableId: string, tripId: string) {
   const api = useApiClient();
-  const positionsQuery = useTripPositions(trackableId, tripId);
 
   return useQuery<TripWithPositions>({
     queryKey: ['trip-with-positions', trackableId, tripId],
     queryFn: async () => {
       const trip = await api.get<Trip>(`/api/trackables/${trackableId}/trips/${tripId}`);
-      return { ...trip, positions: positionsQuery.data ?? [] };
-    },
-    enabled: !!positionsQuery.data,
-    staleTime: 5 * 60 * 1000,
-    retry: 1,
-    placeholderData: positionsQuery.data
-      ? { positions: positionsQuery.data } as TripWithPositions
-      : undefined,
-  });
-}
+      const positions = await api.get<Position[]>(`/api/trackables/${trackableId}/trips/${tripId}/positions`);
 
-export function useDetailedTrip(trip: Trip | null) {
-  const api = useApiClient();
-  const positionsQuery = useTripPositions(trip?.trackableId, trip?.slug);
-
-  return useQuery<TripDetailed>({
-    queryKey: ['trip-detailed', trip?.trackableId, trip?.slug],
-    queryFn: async () => {
-      if (!trip) throw new Error('Trip is required');
-
-      const markers = await api.get<Marker[]>(
-        `/api/trackables/${trip.trackableId}/trips/${trip.slug}/markers`
-      );
-
-      return { ...trip, positions: positionsQuery.data ?? [], markers };
-    },
-    enabled: !!trip && positionsQuery.isSuccess,
-    staleTime: 5 * 60 * 1000,
-    retry: 1,
-    placeholderData: {
-      ...trip,
-      positions: positionsQuery.data ?? [],
-      markers: [],
-    } as TripDetailed,
-  });
-}
-
-// This is ment to be private
-// It is used to avoid duplicate network requests for positions on useDetailedTrip and useTripWithPosition
-function useTripPositions(trackableId?: string, tripId?: string) {
-  const api = useApiClient();
-
-  return useQuery<Position[]>({
-    queryKey: ['trip-positions', trackableId, tripId],
-    queryFn: async () => {
-      if (!trackableId || !tripId) throw new Error('IDs are required');
-      return api.get<Position[]>(`/api/trackables/${trackableId}/trips/${tripId}/positions`);
+      return { ...trip, positions };
     },
     enabled: !!trackableId && !!tripId,
     staleTime: 5 * 60 * 1000,
-    retry: 1,
+    retry: 1
   });
+}
+
+export function useTrip(trip: Trip | null) {
+  const api = useApiClient();
+  return useQuery<TripDetailed>({
+    queryKey: ['trip', trip?.trackableId, trip?.slug],
+    queryFn: async () => {
+      if (!trip?.trackableId || !trip?.slug) throw new Error('IDs are required');
+      const positions = await api.get<Position[]>(`/api/trackables/${trip?.trackableId}/trips/${trip?.slug}/positions`);
+      const markers = await api.get<Marker[]>(
+        `/api/trackables/${trip?.trackableId}/trips/${trip?.slug}/markers`
+      );
+      return { ...trip, positions, markers };
+    },
+    enabled: !!trip?.trackableId && !!trip?.slug,
+    staleTime: 5 * 60 * 1000,
+    retry: 1
+    });
 }
