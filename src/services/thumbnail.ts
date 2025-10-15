0ff0fcf66a5bf83534aea0124d91ffcdd68f27d0
arnovr <arnovanrossum1@gmail.com>
2025-10-15 10:19:41 +0200

    At default use 200x200 thumbnails, and instead of returning full size image use the 2000x2000 resolution. This is way faster

--- src/services/thumbnail.ts
+++ src/services/thumbnail.ts
@@ -3,7 +3,7 @@ import type { Marker } from "@/store/atoms";
 
 export function getMarkerUrlThumbnail(
   marker: Marker,
-  size: string = "100x100",
+  size: string = "200x200",
 ): string {
   if (!marker.hasThumbnail) {
     return getMarkerImage(marker);
@@ -12,7 +12,8 @@ export function getMarkerUrlThumbnail(
 }
 
 export function getMarkerImage(marker: Marker): string {
-  return `${config.markerUrl}/api/trackables/${marker.trackableId}/markers/${marker.markerId}/image`;
+  // return `${config.markerUrl}/api/trackables/${marker.trackableId}/markers/${marker.markerId}/image`;
+  return getMarkerUrlThumbnail(marker, "2000x2000");
 }
 
 export function getTrackableIcon(trackableId: string): string {
