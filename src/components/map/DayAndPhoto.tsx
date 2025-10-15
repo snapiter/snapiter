38ccdd999aa1b160b9f955a5f8d4ab1248c1fe52
arnovr <arnovanrossum1@gmail.com>
2025-10-15 09:15:16 +0200

    Wrap it on big screens

--- src/components/map/DayAndPhoto.tsx
+++ src/components/map/DayAndPhoto.tsx
@@ -11,20 +11,18 @@ export default function ({
   markersLength: number;
 }) {
   return (
-    <p className="mt-2 text-foreground flex items-center space-x-4 text-sm">
-      {/* Duration */}
-      <span className="flex items-center space-x-1 text-muted whitespace-nowrap">
+    <div className="mt-2 text-foreground flex flex-wrap items-center space-x-4 text-sm max-w-full">
+      <span className="flex items-center  space-x-1 text-muted whitespace-nowrap">
         <FaRegCalendar className="w-4 h-4" />
         <span>{formatTripDate(startDate, endDate)}</span>
       </span>
 
-      {/* Photos (only if selected & has photos) */}
       {markersLength > 0 && (
         <span className="flex items-center space-x-1 text-muted">
           <FaCamera className="w-4 h-4" />
           <span>{markersLength}</span>
         </span>
       )}
-    </p>
+    </div>
   );
 }
